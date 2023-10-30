8523e9b15687a0dd1fc0d8af466dcf7cbd652462
jaxCodesCode <143358866+jaxCodesCode@users.noreply.github.com>
2023-10-30 15:41:05 -0700

    vite vercel deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import TodoApp from "@/components/TodoApp";
 import '@/styles/app.scss';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
-    <HashRouter basename={import.meta.env.DEV ? '/' : '/todo-demo-vite/'}>
+    <BrowserRouter>
       <TodoApp />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,12 @@ import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const _dirname = dirname(fileURLToPath(import.meta.url))
-export default defineConfig(({command}) => {
-  const config = {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(_dirname,  './src')
-      }
-    },
-    base: '/'
-  };
-
-  if (command !== 'serve') {
-    config.base = '/todo-demo-vite/'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(_dirname,  './src')
+    }
   }
-
-  return config; 
 })
