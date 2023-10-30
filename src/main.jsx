cf10d09e782a13a28eff270a52eb914d04f4919b
jaxCodesCode <143358866+jaxCodesCode@users.noreply.github.com>
2023-10-30 15:28:52 -0700

    vite gh pages deploy config

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import TodoApp from "@/components/TodoApp";
 import '@/styles/app.scss';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/todo-demo-vite/'}>
+    <HashRouter basename={import.meta.env.DEV ? '/' : '/todo-demo-vite/'}>
       <TodoApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
